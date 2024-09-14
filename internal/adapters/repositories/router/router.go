0b45c97a91e947838caeb897f23368f11eead1f5
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 <vaibhav84334@gmail.com>
2024-09-14 08:56:07 -0700

    Make get posts to be public

--- internal/adapters/repositories/router/router.go
+++ internal/adapters/repositories/router/router.go
@@ -1,10 +1,11 @@
 package router
 
 import (
-	"github.com/gorilla/mux"
 	"post-comment-service/internal/adapters/repositories/http"
 	"post-comment-service/internal/config"
 	"post-comment-service/pkg/middleware"
+
+	"github.com/gorilla/mux"
 )
 
 func NewRouter(cfg *config.Config) *mux.Router {
@@ -18,12 +19,14 @@ func NewRouter(cfg *config.Config) *mux.Router {
 	r.HandleFunc("/register", userHandler.Register).Methods("POST")
 	r.HandleFunc("/login", userHandler.Login).Methods("POST")
 
+	// Public route for listing posts
+	r.HandleFunc("/posts", postHandler.ListPosts).Methods("GET")
+
 	// Protected routes
 	protected := r.PathPrefix("").Subrouter()
 	protected.Use(middleware.AuthMiddleware)
 
 	protected.HandleFunc("/posts", postHandler.CreatePost).Methods("POST")
-	protected.HandleFunc("/posts", postHandler.ListPosts).Methods("GET")
 	protected.HandleFunc("/posts/{id}", postHandler.GetPost).Methods("GET")
 	protected.HandleFunc("/posts/{postID}/comments", postHandler.CreateComment).Methods("POST")
 	protected.HandleFunc("/posts/{postID}/comments", postHandler.GetComments).Methods("GET")
